0336fdf8bde82b40c95d14e7087c3fcadb21939a
MD Farhad Jaman <dev@growthandanalytics.com>
2023-08-30 16:07:48 +0600

    changed control plane

--- integration/rudderstack.js
+++ integration/rudderstack.js
@@ -1,5 +1,5 @@
 import RudderAnalytics from "@rudderstack/rudder-sdk-node"
-const rudderEvent = new RudderAnalytics("2UWhu5SpHYDlnUt0brzFpDxbMst", {
+const rudderEvent = new RudderAnalytics("2UhPw1QWwGX3YM7dXSkYO8ZLY5n", {
   dataPlaneUrl: "https://rudderstack.whgi.net",
 })
 
